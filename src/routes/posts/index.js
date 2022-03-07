72871734d7e645fa8082ae7342fca95fd8b068b1
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2022-03-07 19:37:24 +0000

    update

--- src/routes/posts/index.js
+++ src/routes/posts/index.js
@@ -1,18 +1,25 @@
-import PostModel from "../../database/models/Post.js"
-import User from "../../database/models/User.js"
-import { Router } from "express"
-import multer from "multer"
-import crypto from "crypto"
-import { GridFsStorage } from "multer-gridfs-storage"
-import Grid from "gridfs-stream"
-import { JWTAuthMiddleware } from "../../auth/token.js"
-import path from "path"
-import "dotenv/config"
+import PostModel from "../../database/models/Post.js";
+import User from "../../database/models/User.js";
+import { Router } from "express";
+import mongoose from "mongoose";
+import multer from "multer";
+import crypto from "crypto";
+import { GridFsStorage } from "multer-gridfs-storage";
+import Grid from "gridfs-stream";
+import { JWTAuthMiddleware } from "../../auth/token.js";
+import path from "path";
+import mongo from "mongodb";
+import "dotenv/config";
 
 const postRouter = Router();
+
 const mongoURI = process.env.MONGO_CONNECTION;
 
+const db = mongoURI;
+const gfs = await Grid(db, mongo);
+
 // Create storage engine
+
 const storage = new GridFsStorage({
   url: mongoURI,
   file: (req, file) => {
@@ -21,111 +28,139 @@ const storage = new GridFsStorage({
         if (err) {
           return reject(err);
         }
-        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const filename = buf.toString("hex") + path.extname(file.originalname);
         const fileInfo = {
           filename: filename,
-          bucketName: 'uploads'
+          bucketName: "uploads",
         };
         resolve(fileInfo);
       });
     });
-    }
+  },
 });
 
 const upload = multer({ storage });
 
 // Create Post
 
-postRouter.post("/", JWTAuthMiddleware, upload.single('video'), async (req, res, next) => {
-    const newPost = new PostModel(req.body)
+postRouter.post(
+  "/",
+  JWTAuthMiddleware,
+  upload.single("video"),
+  async (req, res, next) => {
+    const newPost = new PostModel(req.body);
     try {
-        const savedPost = await newPost.save()
-        res.status(200).json(savedPost)
+      const savedPost = await newPost.save();
+      const payload = new FormData();
+      const video = req.video;
+      payload.append("video", video);
+      console.log({ video: req.video });
+      res.status(200).json(savedPost);
     } catch (error) {
-        res.status(500).json(error)
+      res.status(500).json(error);
     }
-})
+  }
+);
 
-// Post Video
+// Get All Videos
 
-postRouter.post("/video", JWTAuthMiddleware, upload.single('video'), async (req, res, next) => {
- // seperate post video route or include it in the post route as a non required option? 
-
-})
+postRouter.get("/media", async (req, res, next) => {
+  try {
+      const videos = await gfs.find('uploads').toArray((err, files) => {
+          if (!files || files.length === 0) {
+              return res.status(200).json({
+                  success: false,
+                  message: 'No files available'
+              });
+          }
+          files.map(file => {
+              if (file.contentType === 'video/mp4') {
+                  file.isVideo = true;
+              }
+          })
+    });
+    res.json(videos);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
 // Get Specific Post
 
 postRouter.get("/:id", async (req, res, next) => {
-    try {
-        const post = await PostModel.findById(req.params.id)
-        res.status(200).json(post)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-})
+  try {
+    const post = await PostModel.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 // Edit Post
 
 postRouter.put("/:id", async (req, res, next) => {
-    try {
-        const post = await PostModel.findById(req.params.id)
-        if (post.userId === req.body.userId) {
-            await PostModel.updateOne({ $set: req.body })
-            res.status(200).json("Post updated successfully!")
-        } else {
-            res.status(403).json("You can only update your own posts.")
-        }
-    } catch (error) {
-        res.status(500).json(error)
+  try {
+    const post = await PostModel.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await PostModel.updateOne({ $set: req.body });
+      res.status(200).json("Post updated successfully!");
+    } else {
+      res.status(403).json("You can only update your own posts.");
     }
-})
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 // Delete Post
 
 postRouter.delete("/:id", async (req, res, next) => {
-    try {
-        const post = await PostModel.findById(req.params.id)
-        if (post.userId === req.body.userId) {
-            await PostModel.deleteOne()
-            res.status(200).json("Post successfully deleted!")
-        } else {
-            res.status(403).json("You can only delete your own posts.")
-        }
-    } catch (error) {
-        res.status(500).json(error)
+  try {
+    const post = await PostModel.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await PostModel.deleteOne();
+      res.status(200).json("Post successfully deleted!");
+    } else {
+      res.status(403).json("You can only delete your own posts.");
     }
-})
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 // Like and Dislike Post
 
 postRouter.put("/:id/like", async (req, res, next) => {
-    try {
-        const post = await PostModel.findById(req.params.id)
-        if (!post.likes.includes(req.body.userId)) {
-            await PostModel.updateOne({ $push: { likes: req.body.userId } })
-            res.status(200).json("Post liked!")
-        } else {
-            await PostModel.updateOne({ $pull: { likes: req.body.userId } })
-            res.status(200).json("Post disliked!")
-        }
-    } catch (error) {
-        res.status(500).json(error)
+  try {
+    const post = await PostModel.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await PostModel.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json("Post liked!");
+    } else {
+      await PostModel.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json("Post disliked!");
     }
-})
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 // Get Timeline Posts
 
-postRouter.get("/timeline"), async (req, res, next) => {
+postRouter.get("/timeline"),
+  async (req, res, next) => {
     try {
-        const currentUser = await User.findById(req.body.userId)
-        const userPosts = await PostModel.find({ userId: currentUser._id })
-        const friendPosts = await Promise.all(currentUser.followings.map((friendId) => {
-            return PostModel.find({ userId: friendId })
-        }))
-        res.json(userPosts.concat(...friendPosts))
+      const currentUser = await User.findById(req.body.userId);
+      const userPosts = await PostModel.find({ userId: currentUser._id });
+      const friendPosts = await Promise.all(
+        currentUser.followings.map((friendId) => {
+          return PostModel.find({ userId: friendId });
+        })
+      );
+      res.json(userPosts.concat(...friendPosts));
     } catch (error) {
-        res.status(500).json(error)
+      res.status(500).json(error);
     }
-}
+  };
 
 export default postRouter;
