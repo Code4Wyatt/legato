be2dc18262ddee7712aec05dcf13f3c342a502d3
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2022-03-24 12:27:10 +0000

    Fixing image post

--- src/routes/posts/index.js
+++ src/routes/posts/index.js
@@ -1,6 +1,6 @@
 import PostModel from "../../database/models/Post.js";
 import User from "../../database/models/User.js";
-import VideoModel from "../../database/models/Video.js"
+import VideoModel from "../../database/models/Video.js";
 import { Router } from "express";
 import mongoose from "mongoose";
 import multer from "multer";
@@ -8,7 +8,7 @@ import crypto from "crypto";
 import { GridFsStorage } from "multer-gridfs-storage";
 import Grid from "gridfs-stream";
 import { JWTAuthMiddleware } from "../../auth/token.js";
-import { v2 as Cloudinary } from "cloudinary"
+import { v2 as Cloudinary } from "cloudinary";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import mongo from "mongodb";
 import "dotenv/config";
@@ -16,101 +16,80 @@ import streamifier from "streamifier";
 import fs from "fs";
 const mongoURI = process.env.MONGO_CONNECTION;
 
-
 Cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_KEY,
-    api_secret: process.env.CLOUD_SECRET
-  })
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_KEY,
+  api_secret: process.env.CLOUD_SECRET,
+});
 
+const cloudinaryStorage = new CloudinaryStorage({
+  cloudinary: Cloudinary,
+  params: {
+    folder: "unison",
+    format: async (req, file) => "png", // supports promises as well
+  },
+});
 
-const storage = new CloudinaryStorage({
-    cloudinary: Cloudinary,
-    params: {
-      folder: 'unison',
-      format: async (req, file) => 'png', // supports promises as well
-    },
-  })
-
-const parser = multer({ storage: storage })
+const parser = multer({ storage: cloudinaryStorage });
 
 const postRouter = Router();
 
 // Create Post
 
-postRouter.post(
-  "/",
-  async (req, res, next) => {
-    try {
-      const newPost = new PostModel(req.body);
-      const savedPost = await newPost.save();
+postRouter.post("/", async (req, res, next) => {
+  try {
+    const newPost = new PostModel(req.body);
+    const savedPost = await newPost.save();
 
-      if (newPost) {
-        res.send({savedPost});
-      }
-
-      console.log(savedPost)
-    } catch (error) {
-      res.status(500).send({message: error.message});
+    if (newPost) {
+      res.send({ savedPost });
     }
+
+    console.log(savedPost);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
   }
-);
+});
 
 // Post Image to Post
 
-postRouter.post("/:postId/image", parser.single('image'), async (req, res, next) => {
-  try {
-    let streamUpload = (req) => {
-        return new Promise((resolve, reject) => {
-          let stream =  Cloudinary.uploader.upload_stream(
-              (error, result) => {
-                if (result) {
-                  resolve(result);
-                  res.write(JSON.parse(result));
-                  console.log(result);
-                } else {
-                  reject(error);
-                  console.log(error);
-                }
-              }
-          );
+postRouter.post("/:postId",
+  parser.single("image"),
+  async (req, res, next) => {
+    try {
+      const getPostById = await PostModel.findById(req.params.postId);
 
-          // ! console.log(stream);
-      streamifier.createReadStream(req.buffer).pipe(stream);
-          // ? createReadSteam error
-        });
-    };
+      if (getPostById) {
+        getPostById.image = req.file.path;
 
-    async function upload(req) {
-        let result = await streamUpload(req.file);
-        console.log(result);
+        await getPostById.save();
+
+        res.status(203).send({ success: true, data: getPostById });
+      } else {
+        res.status(404).send({ success: false, message: "Post not found" });
+      }
+    } catch (error) {
+      res.status(500).send({ success: false, error: error.message });
     }
-
-    upload(req);
-  } catch (error) {
-    res.status(400).send(error);
   }
-
-})
-
-
+);
 
 // Get All Videos
 
 postRouter.get("/media", async (req, res, next) => {
   try {
-      const videos = await gfs.find('uploads').toArray((err, files) => {
-          if (!files || files.length === 0) {
-              return res.status(200).json({
-                  success: false,
-                  message: 'No files available'
-              });
-          }
-          files.map(file => {
-              if (file.contentType === 'video/mp4') {
-                  file.isVideo = true;
-              }
-          })
+    const videos = await gfs.find("uploads").toArray((err, files) => {
+      if (!files || files.length === 0) {
+        return res.status(200).json({
+          success: false,
+          message: "No files available",
+        });
+      }
+      files.map((file) => {
+        if (file.contentType === "video/mp4") {
+          file.isVideo = true;
+        }
+      });
     });
     res.json(videos);
   } catch (error) {
